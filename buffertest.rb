e90484ae905429cd418ec0f5fe30ca0905bab3ef
Andreas S <andreas@andreas-s.net>
2006-05-26 14:30:59 +0000

    this seems to work ok
    git-svn-id: svn://mikrocontroller.net/arm-mp3-aac-player/branches/at91-mmc@82 4b40c21a-61f2-0310-aab4-eff53e755e35

--- buffertest.rb
+++ buffertest.rb
@@ -2,34 +2,35 @@ require 'test/unit/assertions'
 
 include Test::Unit::Assertions
 
-def get_writable_buffer()
-  if @buffers[@wp] == :busy
-    # check if buffer is really busy
-    # dma_address = wp || next_dma_address = wp
-    @buffers[@wp] = :empty
-  end
+def get_read_available()
+  return @n_readable
+end
 
-  if @buffers[@wp] == :empty
+def get_write_available()
+  (MAX_BUFFERS - @n_readable - @n_busy)
+end
+
+def get_writable_buffer()
+#  if @buffers[@wp] == :busy
+#    # check if buffer is really busy
+#    @buffers[@wp] = :empty
+#  end
+
+  if get_write_available > 0
     buffer = @wp
     @wp = (@wp + 1) % MAX_BUFFERS
+    @n_readable += 1
     return buffer
   else
     return false
   end
 end
 
-def set_buffer_ready(i)
-  @buffers[i] = :ready
-end
-
-def set_buffer_busy(i)
-  @buffers[i] = :busy
-end
-
 def get_readable_buffer()
-  if @buffers[@rp] == :ready
+  if get_read_available > 0
     buffer = @rp
     @rp = (@rp + 1) % MAX_BUFFERS
+    @n_readable -= 1
     return buffer
   else
     return false
@@ -37,34 +38,36 @@ def get_readable_buffer()
 end
 
 def show()
-  puts @buffers.inspect
   puts "wp: #{@wp}"
   puts "rp: #{@rp}"
+  puts "n_readable: #{@n_readable}"
+  puts "n_busy: #{@n_busy}"
+  puts "wa: #{get_write_available()}"
+  puts "ra: #{get_read_available()}"
+  puts
 end
 
 MAX_BUFFERS = 4
 @buffers = [:empty] * MAX_BUFFERS
 @wp = 0
 @rp = 0
+@n_readable = 0
+@n_busy = 0
 
-buffer = get_writable_buffer()
-# work with buffer
-set_buffer_ready(buffer)
+puts "START"
+
 show
 
 buffer = get_writable_buffer()
-# work with buffer
-set_buffer_ready(buffer)
+show
+
+buffer = get_writable_buffer()
 show
 
 buffer = get_readable_buffer()
-# start DMA transfer
-set_buffer_busy(buffer)
 show
 
 buffer = get_readable_buffer()
-# start DMA transfer
-set_buffer_busy(buffer)
 show
 
 buffer = get_readable_buffer()
@@ -72,26 +75,28 @@ show
 assert_equal(false, buffer)
 
 buffer = get_writable_buffer()
-# work with buffer
-set_buffer_ready(buffer)
 show
 
 buffer = get_writable_buffer()
-# work with buffer
-set_buffer_ready(buffer)
 show
 
 buffer = get_writable_buffer()
-# work with buffer
-set_buffer_ready(buffer)
 show
 
 buffer = get_writable_buffer()
-# work with buffer
-set_buffer_ready(buffer)
 show
 
 buffer = get_writable_buffer()
-# work with buffer
 show
 assert_equal(false, buffer)
+
+get_readable_buffer()
+@n_busy = 1
+show
+
+get_readable_buffer()
+@n_busy = 2
+show
+
+@n_busy = 0
+show
