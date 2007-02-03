ce3fe68ae59949cffd126801ae25e35e6556acd3
Andreas S <andreas@andreas-s.net>
2007-02-03 15:44:50 +0000

    refactored play_mp3.c
    git-svn-id: svn://mikrocontroller.net/arm-mp3-aac-player/trunk@124 4b40c21a-61f2-0310-aab4-eff53e755e35

--- play_mp3.c
+++ play_mp3.c
@@ -33,8 +33,8 @@
 
 static HMP3Decoder hMP3Decoder;
 static MP3FrameInfo mp3FrameInfo;
-static unsigned char *readPtr;
-static int bytesLeft=0, bytesLeftBeforeDecoding=0, err, offset;
+static unsigned char *read_ptr;
+static int bytes_left=0, bytes_leftBeforeDecoding=0, err, offset;
 static int nFrames = 0;
 static unsigned char *mp3buf;
 static unsigned int mp3buf_size;
@@ -49,8 +49,8 @@ void mp3_init(unsigned char *buffer, unsigned int buffer_size)
 
 void mp3_reset()
 {
-	readPtr = NULL;
-	bytesLeftBeforeDecoding = bytesLeft = 0;
+	read_ptr = NULL;
+	bytes_leftBeforeDecoding = bytes_left = 0;
 	nFrames = 0;
 }
 
@@ -66,79 +66,78 @@ void mp3_free()
 	allocated = 0;
 }
 
+int mp3_refill_inbuffer(FIL *mp3file)
+{
+  int bytes_to_read;
+  
+  iprintf("left: %d. refilling inbuffer...\n", bytes_left);
+  if (bytes_left > 0) {
+    // after fseeking backwards the FAT has to be read from the beginning -> S L O W
+		//assert(f_lseek(mp3file, mp3file->fptr - bytes_leftBeforeDecoding) == FR_OK);
+		// better: move unused rest of buffer to the start
+		// no overlap as long as (1024 <= mp3buf_size/2), so no need to use memove
+		memcpy(mp3buf, read_ptr, bytes_left);
+  }
+  
+  bytes_to_read = mp3buf_size - bytes_left;
+  
+	assert(f_read(mp3file, (BYTE *)mp3buf + bytes_left, bytes_to_read, &bytes_read) == FR_OK);
+	if (bytes_read == bytes_to_read) {
+		read_ptr = mp3buf;
+		offset = 0;
+		bytes_left = mp3buf_size;
+		iprintf("ok. read: %d. left: %d\n", bytes_read, bytes_left);
+		return 0;
+	} else {
+		puts("can't read more data");
+		return -1;
+	}
+}
+
 int mp3_process(FIL *mp3file)
 {
 	int writeable_buffer;
 	WORD bytes_read;
 	
-	if (readPtr == NULL) {
-		assert(f_read(mp3file, (BYTE *)mp3buf, mp3buf_size, &bytes_read) == FR_OK);
-		if (bytes_read == mp3buf_size) {
-			readPtr = mp3buf;
-			offset = 0;
-			bytesLeft = mp3buf_size;
-		} else {
-			puts("can't read more data");
-			return -1;
-		}
+	if (read_ptr == NULL) {
+		if(mp3_refill_inbuffer(mp3file) != 0)
+		  return -1;
 	}
 
-	offset = MP3FindSyncWord(readPtr, bytesLeft);
+	offset = MP3FindSyncWord(read_ptr, bytes_left);
 	if (offset < 0) {
 		puts("Error: MP3FindSyncWord returned <0");
 		
-		// read more data
-		assert(f_read(mp3file, (BYTE *)mp3buf, mp3buf_size, &bytes_read) == FR_OK);
-		if (bytes_read == mp3buf_size) {
-			readPtr = mp3buf;
-			offset = 0;
-			bytesLeft = mp3buf_size;
-			return 0;
-		} else {
-			puts("can't read more data");
-			return -1;
-		}
+		if(mp3_refill_inbuffer(mp3file) != 0)
+		  return -1;
 	}
 
-	readPtr += offset;
-	bytesLeft -= offset;
-	bytesLeftBeforeDecoding = bytesLeft;
+	read_ptr += offset;
+	bytes_left -= offset;
+	bytes_leftBeforeDecoding = bytes_left;
 
 	// check if this is really a valid frame
 	// (the decoder does not seem to calculate CRC, so make some plausibility checks)
-	if (MP3GetNextFrameInfo(hMP3Decoder, &mp3FrameInfo, readPtr) == 0 &&
+	if (MP3GetNextFrameInfo(hMP3Decoder, &mp3FrameInfo, read_ptr) == 0 &&
 		mp3FrameInfo.nChans == 2 &&
 		mp3FrameInfo.version == 0) {
-		debug_printf("Found a frame at offset %x\n", offset + readPtr - mp3buf + mp3file->fptr);
+		debug_printf("Found a frame at offset %x\n", offset + read_ptr - mp3buf + mp3file->fptr);
 	} else {
 		puts("this is no valid frame");
 		// advance data pointer
-		// TODO: handle bytesLeft == 0
-		assert(bytesLeft > 0);
-		bytesLeft -= 1;
-		readPtr += 1;
+		// TODO: handle bytes_left == 0
+		assert(bytes_left > 0);
+		bytes_left -= 1;
+		read_ptr += 1;
 		return 0;
 	}
 
-	if (bytesLeft < 1024) {
-		// after fseeking backwards the FAT has to be read from the beginning -> S L O W
-		//assert(f_lseek(mp3file, mp3file->fptr - bytesLeftBeforeDecoding) == FR_OK);
-		// better: move unused rest of buffer to the start
-		// no overlap as long as (1024 <= mp3buf_size/2), so no need to use memove
-		memcpy(mp3buf, readPtr, bytesLeft);
-		assert(f_read(mp3file, (BYTE *)mp3buf + bytesLeft, mp3buf_size - bytesLeft, &bytes_read) == FR_OK);
-		if (bytes_read == mp3buf_size - bytesLeft) {
-			readPtr = mp3buf;
-			offset = 0;
-			bytesLeft = mp3buf_size;
-			return 0;
-		} else {
-			puts("can't read more data");
-			return -1;
-		}
+	if (bytes_left < 1024) {
+		if(mp3_refill_inbuffer(mp3file) != 0)
+		  return -1;
 	}
 
-	debug_printf("bytesLeftBeforeDecoding: %i\n", bytesLeftBeforeDecoding);
+	debug_printf("bytes_leftBeforeDecoding: %i\n", bytes_leftBeforeDecoding);
 
 	while (dac_fill_dma() == 0);
 	
@@ -150,7 +149,7 @@ int mp3_process(FIL *mp3file)
 	debug_printf("wb %i\n", writeable_buffer);
 
 	//PROFILE_START("MP3Decode");
-	err = MP3Decode(hMP3Decoder, &readPtr, &bytesLeft, dac_buffer[writeable_buffer], 0);
+	err = MP3Decode(hMP3Decoder, &read_ptr, &bytes_left, dac_buffer[writeable_buffer], 0);
 	//PROFILE_END();
 
 	nFrames++;
@@ -159,22 +158,9 @@ int mp3_process(FIL *mp3file)
  		switch (err) {
 		case ERR_MP3_INDATA_UNDERFLOW:
 			puts("ERR_MP3_INDATA_UNDERFLOW");
-			//outOfData = 1;
-			// try to read more data
-			// seek backwards to reread partial frame at end of current buffer
-			// TODO: find out why it doesn't work if the following line is uncommented
-			//mp3file->FilePtr -= bytesLeftBefore;
-			f_read(mp3file, (BYTE *)mp3buf, mp3buf_size, &bytes_read);
-			if (bytes_read == mp3buf_size) {
-				// TODO: reuse writable_buffer
-				readPtr = mp3buf;
-				offset = 0;
-				bytesLeft = mp3buf_size;
-				puts("indata underflow, reading more data");
-			} else {
-				puts("can't read more data");
-				return -1;
-			}
+      bytes_left = 0;
+			if(mp3_refill_inbuffer(mp3file) != 0)
+  		  return -1;
 			break;
 			
  		case ERR_MP3_MAINDATA_UNDERFLOW:
@@ -185,9 +171,9 @@ int mp3_process(FIL *mp3file)
  		default:
  			iprintf("unknown error: %i\n", err);
  			// skip this frame
- 			if (bytesLeft > 0) {
- 				bytesLeft --;
- 				readPtr ++;
+ 			if (bytes_left > 0) {
+ 				bytes_left --;
+ 				read_ptr ++;
  			} else {
  				// TODO
  				assert(0);
